2b8d0f7c88b784c87986baef187db601d5ae7864
Devendra Gurung <111891780+learnerdev875@users.noreply.github.com>
2023-12-19 04:21:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //this treats all JS code as newer version. In newer js, classes, modules, arrow functions etc are introduced.
+
+// alert("hello")  //cannot use in node  because its not running in browser
+
+//code should be readable so that others and developers from the future will easily understand it
+
+/*
+    Primitive Types
+    1. number => range  from 2 to power 53
+    2. bigint
+    3. string
+    4. boolean
+    5. null => standalone value. type of null is object
+    6. undefined => variable declared but yet to be assigned with a value
+    7. symbol => unique and immutable premitive value and may be used as the key of an object property
+
+    Non-primitive Type
+    1. object
+
+    typeof is used to check the type of a data
+*/
+
