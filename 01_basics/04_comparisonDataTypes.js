25d4565eae8d8239e33cfda1b616d049ea8c5af9
Devendra Gurung <111891780+learnerdev875@users.noreply.github.com>
2023-12-22 11:13:27 +0000

    comparision operation

---
+++ 01_basics/04_comparisonDataTypes.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1)
+// console.log("02" < 1)
+
+// console.log(null < 0)
+console.log(null == 0) // here, null does not equal to 0
+//  console.log(null >= 0)
+
+console.log(Number(undefined)) //NaN
+
+// == equality check and >, <, <=, >= are comparison checks === strict equality check and checks data types in addition to values
+
+// Type coercion means automatic or impliction conversion of data types
